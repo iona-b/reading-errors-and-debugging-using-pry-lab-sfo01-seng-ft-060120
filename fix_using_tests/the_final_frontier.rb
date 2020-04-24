e8dae54478916184bc7bbfa33b58aee8d4e60c23
Iona <ionabrabender@gmail.com>
2020-04-24 21:18:54 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,21 +1,13 @@
-require 'pry'
-binding.pry
 def generate_star_date
-  binding.pry
   (rand(100000) + 400000) / 10.0
-  binding.pry
 end
 
 
 def state_log(star_date)
-  binding.pry
   "Captain's Log, star date #{star_date}."
 end
 
 def engage
   date = generate_star_date
-  binding.pry
   puts state_log(date)
-  binding.pry
-  binding.pry
 end
